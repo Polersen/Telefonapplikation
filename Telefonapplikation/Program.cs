c746ea829a215303a11055a46867bcebfaeb1413
Anton Örtlund <anton.ortlund@gmail.com>
2023-10-16 15:32:13 +0200

    Klass Person med mera.

--- Telefonapplikation/Program.cs
+++ Telefonapplikation/Program.cs
@@ -2,6 +2,29 @@
 {
     internal class Program
     {
+        public class Person
+        {
+            public string Name { get; set; }
+            public string Phone { get; set; }
+            public string Address { get; set; }
+
+            public Person(string name, string phone, string address)
+            {
+                this.Name = name;
+                this.Phone = phone;
+                this.Address = address;
+            }
+
+            public void Print()
+            {
+                Console.WriteLine($"Namn: {this.Name}\n" +
+                                  $"Telefonnummer: {this.Phone}\n" +
+                                  $"Address: {this.Address}");
+            }
+        }
+
+        public static List<Person> personList = new List<Person>(); //Eventuellt ändra till private senare
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello and welcome to the address list.");
@@ -14,6 +37,10 @@
                 if (command == "help")
                 {
                     Console.WriteLine("Sorry, not implemented!");
+                }
+                else if (command == "load")
+                {
+                    // Loadgrenen: här lägg in kod
                 }
                 else if (command == "exit")
                 {
