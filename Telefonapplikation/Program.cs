e076ff7b6fe97453261a65d9f836d1811818fd52
Anton Örtlund <anton.ortlund@gmail.com>
2023-10-16 14:39:50 +0200

    Skelettkod

--- Telefonapplikation/Program.cs
+++ Telefonapplikation/Program.cs
@@ -1,10 +1,30 @@
-﻿namespace Telefonapplikation
+﻿namespace AddressList
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hej och välkommen till adresslistan.");
+            Console.WriteLine("Skriv 'hjälp' för hjälp!");
+            string command;
+            do
+            {
+                Console.Write("Kommando: ");
+                command = Console.ReadLine();
+                if (command == "hjälp")
+                {
+                    Console.WriteLine($"Tyvärr ej implementerat!");
+                }
+                else if (command == "sluta")
+                {
+
+                }
+                else
+                {
+                    Console.WriteLine($"Okänt kommando: {command}");
+                }
+            } while (command != "sluta");
+            Console.WriteLine("Hej då!");
         }
     }
 }
